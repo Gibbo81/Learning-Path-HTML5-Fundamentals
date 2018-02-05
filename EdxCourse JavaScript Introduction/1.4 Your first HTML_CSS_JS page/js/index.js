b2974bda7827b0c67150780135eea36e8284204a
Giuliano Masi <masigiuliano@yahoo.it>
2018-02-05 09:11:56 +0100

    complete example

--- EdxCourse JavaScript Introduction/1.4 Your first HTML_CSS_JS page/js/index.js
+++ EdxCourse JavaScript Introduction/1.4 Your first HTML_CSS_JS page/js/index.js
@@ -10,7 +10,20 @@ var parameters = {
 
 function plot(){
   var color = document.querySelector("#color").value;
+  var fun = document.querySelector("#function").value;
   //with this command i take the data from the html and overwrite default parameter
   parameters.data[0].color=color;
+  parameters.data[0].fn=fun;
+
+  var xmin = document.querySelector("#xMin").value; 
+  var xMax = document.querySelector("#xMax").value; 
+  parameters.xAxis.domain = [xmin, xMax];
+
+  var ymin = document.querySelector("#yMin").value; 
+  var yMax = document.querySelector("#yMax").value; 
+  parameters.yAxis.domain = [ymin, yMax];
+  
+
+
   functionPlot(parameters);
 }
