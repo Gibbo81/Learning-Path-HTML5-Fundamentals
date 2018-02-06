3fc307b3760b0180a3b41342f2f2bd88c32fd1da
Giuliano Masi <masigiuliano@yahoo.it>
2018-02-06 08:59:45 +0100

    start chapter 1.5 Variables, values, functions, operators and expressions

---
+++ EdxCourse JavaScript Introduction/1.5 Variables, values, functions, operators and expressions/01/js/index.js
@@ -0,0 +1,14 @@
+
+//variables names are in camel case convention
+//JavaScript is weakly typed
+var myName;
+let YourName;
+
+
+myName="Pippus";
+
+console.log(myName)
+
+//Constants naming convention: use uppercase letters with underscores. 
+const TIME_LIMIT = 50;
+
