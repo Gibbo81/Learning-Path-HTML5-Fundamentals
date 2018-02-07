f788c00681ae1c41aa14af6c8d21fe76e681b654
Giuliano Masi <masigiuliano@yahoo.it>
2018-02-07 09:29:31 +0100

    variables scope

--- EdxCourse JavaScript Introduction/1.5 Variables, values, functions, operators and expressions/01/js/index.js
+++ EdxCourse JavaScript Introduction/1.5 Variables, values, functions, operators and expressions/01/js/index.js
@@ -3,12 +3,24 @@
 //JavaScript is weakly typed
 var myName;
 let YourName;
-
+//this outside any function are global variables
 
 myName="Pippus";
 
 console.log(myName)
 
+//Inside a function:
+//var always defines a function local local to the whole function 
+//let instead have as scope only the bloc of instruction where it's created
+function f1(){
+  if(true){
+    let b=4;
+  }
+  console.log(b);  //errror!!!
+}
+
+
 //Constants naming convention: use uppercase letters with underscores. 
 const TIME_LIMIT = 50;
 
+f1();
