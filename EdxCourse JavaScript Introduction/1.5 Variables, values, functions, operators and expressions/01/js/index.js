6d3b67ff2cf5dd48da6532e011760ab5f7e60265
Giuliano Masi <masigiuliano@yahoo.it>
2018-02-09 09:00:24 +0100

    complete 1.5

--- EdxCourse JavaScript Introduction/1.5 Variables, values, functions, operators and expressions/01/js/index.js
+++ EdxCourse JavaScript Introduction/1.5 Variables, values, functions, operators and expressions/01/js/index.js
@@ -10,7 +10,7 @@ myName="Pippus";
 console.log(myName)
 
 //Inside a function:
-//var always defines a function local local to the whole function 
+//var always defines a function variable local to the whole function 
 //let instead have as scope only the bloc of instruction where it's created
 function f1(){
   if(true){
@@ -19,6 +19,22 @@ function f1(){
   console.log(b);  //errror!!!
 }
 
+//function always return a value. if a return statment is not present then it will return
+//always undefined 
+function showandtype(par){
+  console.log(par);
+  console.log(typeof(par));
+}
+
+//function can have a variable numbers of anspecified arguments, an array named "argument" is 
+//created automatically in each function, it contains all the call parameters of the function:
+function newsum(){
+	var result=0;
+	for (var i = arguments.length - 1; i >= 0; i--) {
+		result += arguments[i];
+	}
+	return result;
+}
 
 //Constants naming convention: use uppercase letters with underscores. 
 const TIME_LIMIT = 50;
@@ -27,10 +43,12 @@ const TIME_LIMIT = 50;
 
 //data type number - string - boolean - undefined - null(object) - object 
 var x=10 ;
-console.log(typeof x);  //number
+console.log(typeof x);  //number float number
 
 x="Micheal" ;
 console.log(typeof x);  //string
+//A String number in an arithmetic expression is converted to Number, unless the formula 
+//is a pure addition.
 
 x=true ;
 console.log(typeof x);  //boolean
@@ -46,19 +64,50 @@ of no value
 
 y = null;
 console.log(typeof y);  //object
-if ( y === undefined) {console.log("Yes it's udefined");} //false null is 
+if ( y === undefined) {console.log("Yes it's udefined");} //false null isn't udefined
 
 /*
 Use === Instead of == JavaScript utilizes two different kinds of equality operators: === | 
-!== and == | != It is considered best practice to always use the former set when comparing. 
-"If two operands are of the same type and value, then === produces true and !== produces false.- 
-JavaScript: The Good Parts However, when working with == and !=, you'll run into issues when 
-working with different types. In these cases, they'll try to coerce the values, unsuccessfully.
+!== and == | != It is considered best practice to always use the first set when comparing. 
+"If two operands are of the same type and value, then === produces true and !== produces false.
+However, when working with == and !=, you'll run into issues when working with different types.
+In these cases, they'll try to coerce the values, unsuccessfully.
 */
 var persona = {name: 'Micheal', surname : "pippo"}; //object
 console.log(typeof persona);
 
 var array = [1,2,3,4];
 console.log(typeof array);  //object
+//If you want to add a new element at the end of an array, use the index equal to the length 
+//of the array
+var l = array.length;
+array[l]=5;
+console.log(array[l]);
+//equivalent to
+array.push(6)
+console.log(array[array.length-1]);
+
+/*
+Numbers variable have 3 specials value : +Infinity | -Infinity | NaN
+Infinity (or +Infinity) represents all number values greater than 1.79769313486231570e+308 
+and -Infinity represents values smaller than -1.79769313486231570e+308. Nan represents 
+not-a-number values,
+*/
+var n = 0/0 //NaN
+showandtype(n); //number
+
+n = 3/0;
+showandtype(n); //Infiniti - number
+
+//Any operation with Infinity gives Infinity as a result:
+n = n-999999999999
+showandtype(n); //Infiniti - number
+
+n = n/n
+var u = showandtype(n); //Infiniti - number
+console.log(u);
+
+console.log(newsum(1,2,3,4,5,6,7,8,9,10));
+
 
 f1();
