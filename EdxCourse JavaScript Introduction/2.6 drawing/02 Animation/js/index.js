8800a1cdd376ef1457abad938bbc4e1057bc7b96
Giuliano Masi <masigiuliano@yahoo.it>
2018-03-15 08:48:14 +0100

    start working on animation

--- EdxCourse JavaScript Introduction/2.6 drawing/02 Animation/js/index.js
+++ EdxCourse JavaScript Introduction/2.6 drawing/02 Animation/js/index.js
@@ -1,104 +1,53 @@
-var canvas, ctx, w, h; 
+/*
+A typical animation loop will do the following at regular intervals:
 
-window.onload = init;
+Clear the canvas
+Draw graphic objects / shapes
+Move graphic shapes / objects
+Go to step 1
 
-function init(){
-	canvas = document.querySelector("#myCanvas");
-	ctx = canvas.getContext('2d');
-	//filled rectangle
-	ctx.fillStyle = 'red';
-	ctx.fillRect(10,10,30,30);
-
-    // wireframe rectangle
-    ctx.strokeStyle = 'green';
-    ctx.lineWidth = 4;
-    ctx.strokeRect(100, 40, 40, 40);
-  
-    // fill circle, will use current ctx.fillStyle
-    ctx.beginPath();
-    ctx.arc(60, 60, 10, 0, 2*Math.PI);
-    ctx.fill();
-  
-    // some text
-    ctx.fillStyle = "purple";
-    ctx.font = "20px Arial";
-    ctx.fillText("Hello!", 60, 20);
-
-	drawSecondCanvas();
-
-	canvas = document.querySelector("#MonsterCanvas");
-	ctx = canvas.getContext("2d");
-	w= canvas.width;
-	h= canvas.height;
-	drawMyMonster(100, 60);
-}
-
-
-
-function drawSecondCanvas(){
-	canvas = document.querySelector("#myCanvasTwo");
-	ctx = canvas.getContext("2d");
-	w= canvas.width;
-	h= canvas.height;
-	drawFilledRectangle(10, 10, 20, 20, "green");
-	drawFilledCircle(100, 100, 15, "green");
-}
-
-function drawFilledRectangle(x, y, width, height, color){
-	/*When a function changes anything to the "global context": filled or stroke color, 
-	  line width, or the position of the coordinate system (located by default in 0, 0, 
-	  at the top left of the canvas), then it is good practice to save this context at 
-	  the beginning of the function, with a call to ctx.save(), and to restore it at the
-	   end of the function, with a call to ctx.restore(). In this way, any change to the
-	   "global context" won't have any effect outside of the function.
-	   We used also ctx.translate(x, y) in order to move the rectangle and the circle 
-	   (look, they have been drawn at x=0, y=0, but as we translate the origin of the 
-	   coordinate system with ctx.translate, the shapes are located in x, y on in the canvas). 
-	*/
-	ctx.save();
-
-	ctx.translate(x,y);
-	ctx.fillStyle=color;
-	ctx.fillRect(0, 0, width, height);	
-	//sing coordinates relative to 0, 0, the whole set of shapes will be translated by the 
-	//call to ctx.translate(x, y)
-
-	ctx.restore();
-}
-
-function drawFilledCircle(x, y, radius, color) {
-    ctx.save();
-  	ctx.translate(x, y);
-  
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(0, 0, radius, 0, 2*Math.PI);
-    ctx.fill();
- 
-    ctx.restore();
-}
-
+The trick is to write a function (drawMyMonster, and at the end of this function, to 
+ask the browser to call it again in 1/60th of a second if possible
+*/
 function drawMyMonster(x, y) {
-    // draw a big monster !
-    // head
     ctx.save();
     ctx.translate(x, y);
-  
     ctx.strokeRect(0, 0, 100, 100);
-  
-    // eyes
     ctx.fillRect(20, 20, 10, 10);
     ctx.fillRect(65, 20, 10, 10);
-  
-    // nose
-    ctx.strokeRect(45, 40, 10, 40);
-  
-    // mouth
-   ctx.strokeRect(35, 84, 30, 10);
-  
-   // teeth
-   ctx.fillRect(38, 84, 10, 10);
-   ctx.fillRect(52, 84, 10, 10);
-
+    ctx.strokeRect(45, 40, 10, 40);   ctx.strokeRect(35, 84, 30, 10);
+    ctx.fillRect(38, 84, 10, 10);
+    ctx.fillRect(52, 84, 10, 10);
    ctx.restore();
 }
+
+var canvas, ctx, w, h; 
+var xMonster = 10;
+var yMonster = 10;
+var monsterSpeed = 3;
+
+window.onload = function init(){
+    canvas = document.querySelector("#myCanvas");
+    w = canvas.width;
+    h = canvas.height;
+    ctx = canvas.getContext('2d');
+
+    mainLoop();
+}
+
+function mainLoop(){
+    //1 clear
+    ctx.clearRect(0,0,w,h);
+
+    //2 draw
+    drawMyMonster(xMonster,yMonster)
+
+    //3 move the monster
+    xMonster += monsterSpeed;
+    if ((xMonster + 100> w)||(xMonster<0)) {
+        monsterSpeed = -monsterSpeed;
+    }
+
+    requestAnimationFrame(mainLoop);
+}
+
