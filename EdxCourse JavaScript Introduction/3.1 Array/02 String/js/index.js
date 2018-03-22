46654dc0dbb3c617a6c5ed292c179ec98a8671da
Giuliano Masi <masigiuliano@yahoo.it>
2018-03-22 09:00:29 +0100

    complete 3.2 iterators

--- EdxCourse JavaScript Introduction/3.1 Array/02 String/js/index.js
+++ EdxCourse JavaScript Introduction/3.1 Array/02 String/js/index.js
@@ -15,16 +15,79 @@ console.log(s);
 
 function start(){
     let a = ['a','b','c'];
-    a.forEach(function(value, index, array){
+    a.forEach(function(value, index, array){ //1:current element, 2:elemnt's index, 3:array itself
      document.body.innerHTML += value + ' is at index: ' + index + ' from an array of '+ array.length +'<br>';
     });  
 
     //otherwise
     for (let i=0; i <a.length; i++){
         document.body.innerHTML += a[i] + '<BR>'
-    }
+    };
+
+    addImages();
 
 }
 
+let myPicturesArray = [
+    {
+        "albumId": 1,
+        "id": 1,
+        "title": "accusamus beatae ad facilis cum similique qui sunt",
+        "url": "http://placehold.it/600/92c952",
+        "thumbnailUrl": "http://placehold.it/150/92c952"
+    },
+    {
+        "albumId": 1,
+        "id": 2,
+        "title": "reprehenderit est deserunt velit ipsam",
+        "url": "http://placehold.it/600/771796",
+        "thumbnailUrl": "http://placehold.it/150/771796"
+    },
+    {
+        "albumId": 2,
+        "id": 51,
+        "title": "non sunt voluptatem placeat consequuntur rem incidunt",
+        "url": "http://placehold.it/600/8e973b",
+        "thumbnailUrl": "http://placehold.it/150/8e973b"
+    },
+    {
+        "albumId": 2,
+        "id": 52,
+        "title": "eveniet pariatur quia nobis reiciendis laboriosam ea",
+        "url": "http://placehold.it/600/121fa4",
+        "thumbnailUrl": "http://placehold.it/150/121fa4"
+    },
+    {
+        "albumId": 3,
+        "id": 127,
+        "title": "magnam quia sed aspernatur",
+        "url": "http://placehold.it/600/74456b",
+        "thumbnailUrl": "http://placehold.it/150/74456b"
+    },
+    {
+        "albumId": 3,
+        "id": 128,
+        "title": "est facere ut nam repellat numquam quia quia eos",
+        "url": "http://placehold.it/600/b0931d",
+        "thumbnailUrl": "http://placehold.it/150/b0931d"
+    }
+];
+
+//let's create the image
+function addImages(){
+    var p = document.querySelector('#images');
+    p.innerHTML += "create directly html for images <br>"
+    myPicturesArray.forEach(function(i){
+        p.innerHTML += "<img src='"+ i.thumbnailUrl +"'>"
+    });    
+
+    myPicturesArray.forEach(function(i){
+        let image = document.createElement("img");
+        image.src = i.thumbnailUrl;
+        image.alt = i.title;
+        document.body.append(image);
+    });    
+
+}
 
 
