94ca84f2132d7ebfa1f6b5f108da24a72680ca27
Giuliano Masi <masigiuliano@yahoo.it>
2018-04-11 08:51:16 +0200

    sounds API

---
+++ EdxCourse JavaScript Introduction/3.3 HTML5 multimedia and JavaScript API/03 Music/js/index.js
@@ -0,0 +1,20 @@
+
+
+
+function play(){
+	var player = document.querySelector("#audioPlayer");
+	player.play();
+}
+
+
+function pause(){
+	var player = document.querySelector("#audioPlayer");
+	player.pause();
+}
+
+
+
+
+
+
+
