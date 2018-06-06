711f5d1912583f22d9b34f7d5655ca3d9168d58a
Giuliano Masi <masigiuliano@yahoo.it>
2018-06-06 08:58:49 +0200

    complete promise

--- EdxCourse JavaScript Introduction/5.4 Json/03 Promise/js/index.js
+++ EdxCourse JavaScript Introduction/5.4 Json/03 Promise/js/index.js
@@ -1,35 +1,65 @@
+var queryURL = "https://jsonplaceholder.typicode.com/users";
 
-function promising(){//converting XMLHttpRequest to a promise-based task
-  //return a promise
-  var queryURL = "https://jsonplaceholder.typicode.com/users";
-  return new promise(function(resolve, reject){
+
+function promising(){
+  get().then(function(response){
+            console.log("ok 1° then");
+            console.log(response);
+            return JSON.parse(response);
+          })
+        .then(function(users){
+            console.log("ok 2° then");
+            console.log(users);
+          })
+        .catch(function(error){
+            console.log('Error during fetch: ' + error.message);
+          })
+
+  promiseWithoutSureSync(false).then(function(response){
+       console.log("continuation withoout API call");
+       console.log(response);
+  });
+  promiseWithoutSureSync(true).then(function(response){
+        console.log("continuation with API call");
+        console.log(response);
+  });
+
+}
+
+function get(){//converting XMLHttpRequest to a promise-based task
+               //return a promise  
+  return new Promise(function(resolve, reject){
     var req = new XMLHttpRequest();
+  
     req.open("GET", queryURL);
+    
     req.onload = function(){// This is called even on 404 etc so check the status
       if(req.status===200){
         resolve(req.response);
       }
       else{  //reject
-        reject(Error(req.statuText));
+        reject(Error(req.response));
       }  
-
+    }
+    
+    req.onerror = function(){
+      reject(Error(req.statuText));
     }
 
-  })
+    req.send();
+  });
   //It's up to the developer to manually call resolve or reject within 
   //the body of the callback based on the result of their given task  
-
-
-
 }
 
+/*Sometimes you don't need to complete an async tasks within the promise  if it's 
+possible (NOT SURE!) that an async action will be taken returning a promise will be 
+best so that you can always count on a promise coming out of a given function*/
+function promiseWithoutSureSync(isApiToCall){
+  if (!isApiToCall) {
+    return Promise.resolve("nothing to do :-)")
+  }  
 
-
-
-function search(){
-	var queryURL = "https://jsonplaceholder.typicode.com/users";
-
-  //Only the first parameter, the URL, is required
   var request = new Request(queryURL,{
     method: 'GET',
     redirect:'follow',
@@ -38,43 +68,16 @@ function search(){
     })
   });
 
-  fetch(request)
-    .then(function(response){
-      //the resulting callback data has a json method for converting the raw data 
-      //to a JavaScript object
-      return response.json();  
-    })
-    .then(function(users){
-      displayUsersAsTable(users)      
-    })
-    .catch(function (error) {
-        console.log('Error during fetch: ' + error.message);
-    });
+  return fetch(request)
+        .then(function(response){
+          console.log("called api");
+          return response.json()
+        })
+        .catch(function (error) {
+            console.log('Error during fetch: ' + error.message);
+        });
 }
 
 
-/*
-function displayUsersAsTable(users){
-    var usersDiv = document.querySelector("#users");
-    usersDiv.innerHTML = "";
-  
-    // creates and populate the table with users
-    var table = document.createElement("table");
-          
-    // iterate on the array of users
-    users.forEach(function(currentUser) {
-        // creates a row
-        var row = table.insertRow();
-        // insert cells in the row
-        var nameCell = row.insertCell();
-        nameCell.innerHTML = currentUser.name;
-        var cityCell = row.insertCell();
-        cityCell.innerHTML = currentUser.address.city;
-     });
-  
-     // adds the table to the div
-     usersDiv.appendChild(table);
-}
-*/
 
 
