6a5d0c18918f6af3591ef3882cafd2f6d106577c
Giuliano <masigiuliano@yahoo.it>
2019-09-08 20:19:22 +0200

    Complete chapter 3

--- React/ReactQuickly/Chapter3/01my-app/src/index.js
+++ React/ReactQuickly/Chapter3/01my-app/src/index.js
@@ -8,16 +8,25 @@ class ProfileLink extends React.Component{
     return (b) ? 'http://webapplog.com' : 'https://github.com/'
   }
   render(){
+    let bigFontSize = {fontSize: '20pt'}  //It's a javascript object
     return (
       <div>
         {/* Just like a JS comment */}
         <a href = {this.props.url}
           title = {this.props.label}
           target = "_blank"
+          className = 'pippo'
         >
+          {/*React and JSX accept any attribute that’s a standard HTML attribute, except class and for. Use className and htmlFor instead.*/}
           Profile
         </a>
-        <a href ={this.getUrl(this.props.b)}> {this.getUrl(this.props.b)}</a>
+        <a href ={this.getUrl(this.props.b)}> {(this.props.b) ? 'http://webapplog.com' : 'https://github.com/'}</a> {/*Two different ways to make choice inside JSX*/}
+        <br/>
+        <span
+          style={{borderColor: 'red', borderWidth: 1, borderStyle: 'solid'}}>
+          {[<span>&copy;&mdash;&ldquo;</span>]}</span>
+        <br/>
+        <span style={bigFontSize}>{<span>&copy;&mdash;&ldquo;</span>}</span> {/* equivalent to style={{fontSize: '30pt'}}*/}
       </div>
     );
   }
@@ -31,7 +40,8 @@ class HelloWorld extends React.Component {
       <div>
         <h1>1. Hello world {now} </h1>
         {helloWorldReactElement}
-        <ProfileLink url='https://www.gazzetta.it/' label='Profile for Azat' b='true'/>
+        <ProfileLink url='https://www.gazzetta.it/' label=' Profile for Azat' b={true}/> {/*boolean attribute value must be set in the JavaScript expression for example, use {false} */}
+        <ProfileLink url='https://github.com/' label=' Profile for Azat' b={false}/>
       </div>
 
     )
