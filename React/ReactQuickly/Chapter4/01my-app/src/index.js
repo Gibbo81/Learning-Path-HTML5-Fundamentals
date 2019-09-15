b1aa26d9b43ee15988cdfd93c262ef638d6a1d95
Giuliano <masigiuliano@yahoo.it>
2019-09-15 17:12:23 +0200

    Complete chapter4

--- React/ReactQuickly/Chapter4/01my-app/src/index.js
+++ React/ReactQuickly/Chapter4/01my-app/src/index.js
@@ -2,22 +2,81 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function HelloWorld(props){   //Stateless components can e view as simple function instead of object
+    return (
+      <h1 {...props}>
+        Hello {props.name} world!!
+      </h1>)
+}
+
+function DigitalDisplay(props){
+  return <div>{props.currentTime}</div>
+}
+
+function AnalogDisplay(props) {
+  let date = new Date(props.time);
+  let dialStyle = {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    width: 200,
+    height: 200,
+    borderRadius: 20000,
+    borderStyle: 'solid',
+    borderColor: 'black'
+  };
+  let secondHandStyle = {
+    position: 'relative',
+    top: 100,
+    left: 100,
+    border: '1px solid red',
+    width: '40%',
+    height: 1,
+    transform: 'rotate(' + (date.getSeconds() / 60 * 360 - 90).toString() + 'deg)',
+    transformOrigin: '0% 0%',
+    backgroundColor: 'red'
+  };
+  let minuteHandStyle = {
+    position: 'relative',
+    top: 100,
+    left: 100,
+    border: '1px solid grey',
+    width: '40%',
+    height: 3,
+    transform: 'rotate(' + (date.getMinutes() / 60 * 360 - 90).toString() + 'deg)',
+    transformOrigin: '0% 0%',
+    backgroundColor: 'grey'
+  };
+  let hourHandStyle = {
+    position: 'relative',
+    top: 92,
+    left: 106,
+    border: '1px solid grey',
+    width: '20%',
+    height: 7,
+    transform: 'rotate(' + (date.getHours() / 12 * 360 - 90).toString() + 'deg)',
+    transformOrigin: '0% 0%',
+    backgroundColor: 'grey'
+  };
+  return (
+    <div>
+      <div style={dialStyle}>
+        <div style={secondHandStyle}/>
+        <div style={minuteHandStyle}/>
+        <div style={hourHandStyle}/>
+      </div>
+    </div>
+  );
+};
 
 class Clock extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      currentTime : (new Date()).toLocaleString()
+      currentTime : (new Date()).toString()
     };
     this.launchClock();
   }
-
-  pippo(){
-    console.log('Update Time');
-    this.setState({
-      currentTime :(new Date()).toLocaleString()
-    })
-  };
 
   launchClock(){
     /* Different way without arrowfunction but using pippo instead
@@ -26,15 +85,29 @@ class Clock extends React.Component {
     setInterval(() => {  //Timer that run each 1000 milliseconds
       console.log('Update Time');
       this.setState({         //React merges the data with current states and calls render(). After that, React calls callback
-        currentTime :(new Date()).toLocaleString()
+        currentTime :(new Date()).toString()
       })
     }, 1000);
   };
 
   render(){
     console.log('Rendering Clock...')
-    return <div>{this.state.currentTime}</div>
+    return (
+      <div>
+        <AnalogDisplay time={this.state.currentTime}/>
+        <DigitalDisplay currentTime={this.state.currentTime}/>
+        <HelloWorld name=':-)'/>
+      </div>
+      )
   }
+
+
+  pippo(){
+    console.log('Update Time');
+    this.setState({
+      currentTime :(new Date()).toLocaleString()
+    })
+  };
 }
 
 ReactDOM.render(
