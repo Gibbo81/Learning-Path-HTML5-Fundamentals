91a6c83ec8b798076d8c84cb1b8a63595e9184cf
Giuliano <masigiuliano@yahoo.it>
2019-10-06 18:32:40 +0200

    Complete chapter 7

--- React/ReactQuickly/Chapter07/03UncontrolledForm/src/index.js
+++ React/ReactQuickly/Chapter07/03UncontrolledForm/src/index.js
@@ -32,11 +32,48 @@ class UnControlledInputWithChangeCapture extends React.Component{
   }
 }
 
+class TotallyUncontrolledInput extends React.Component{
+  handleComment(event){
+    let fD = ReactDOM.findDOMNode;  //function used to read a node
+    console.log('Mail:', fD(this.refs.mail).value);
+    console.log('Comment:',fD(this.refs.comments).value);
+    //Old way always valid :-)
+    var m = document.querySelector('#mailId');
+    var c = document.querySelector('#commentsId');
+    console.log('OLD STYLE Mail:', m.value);
+    console.log('OLD STYLE Comment:', c.value);
+  }
+
+  render(){
+    return(
+      <div>
+        <input
+          type='text'
+          ref='mail'  //necessary to read a node using ReactDOM.findDOMNode
+          id ='mailId'
+          placeholder="hi@azat.co"
+        />
+        {/*A placeholder property is a visual aid to show an example of what to enter*/}
+        {/*Different from default value because doesn't set the value of the input tag*/}
+        <input
+          type='text'
+          ref='comments'
+          id ='commentsId'
+        /><br/>
+        <input type='button' onClick={this.handleComment.bind(this)} value='save'/>
+        <br/>
+        <input type='text' value='Read only :-('/>
+        <input type='text' defaultValue='With default value no-Read only :-)'/>{/*whith defaultValue sets the value and lets users modify form elements.*/}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <div>
     <UnControlledInputWithChangeCapture/>
     -----------------------------------
+    <TotallyUncontrolledInput/>
   </div>,
   document.getElementById('content')
 );
