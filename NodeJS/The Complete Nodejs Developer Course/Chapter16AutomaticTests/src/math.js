ae40350a20467aeaf1c3bcc4a8ff65bf98150088
Giuliano <masigiuliano@yahoo.it>
2020-07-22 08:39:15 +0200

    async testing with jester

--- NodeJS/The Complete Nodejs Developer Course/Chapter16AutomaticTests/src/math.js
+++ NodeJS/The Complete Nodejs Developer Course/Chapter16AutomaticTests/src/math.js
@@ -3,6 +3,21 @@ const calculateTip = (total, tipPercentual = .25) =>{
     return total + tip
 }
 
+const fahrenheitToCelsius = (temp) => (temp-32) / 1.8
+
+const celsiusToFahrenheit = (temp) => (temp * 1.8) + 32
+
+const addAsync = (a,b) => {
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{            
+            resolve(a+b)
+        }, 2000)  
+    })    
+}
+
 module.exports = {
-    calculateTip    //simplified version ===   calculateTip:calculateTip
+    calculateTip,    //simplified version ===   calculateTip:calculateTip
+    fahrenheitToCelsius,
+    celsiusToFahrenheit,
+    addAsync
 }
--- NodeJS/The Complete Nodejs Developer Course/Chapter16AutomaticTests/tests/first.test.js
+++ NodeJS/The Complete Nodejs Developer Course/Chapter16AutomaticTests/tests/first.test.js
@@ -1,6 +1,8 @@
 /*the file name is important:
 Jester looks for file with the followinf format: *.test.js
 this file math the convention
+
+To run the test simple 'npm test' inside the terminal
 */
 
 const math = require ('../src/math')
@@ -24,4 +26,40 @@ test('calculateTip_defaultPercentual_ReturnCorrrectTip', () => {
     const total = math.calculateTip(10)
     expect(total).toBe(12.5)
 })
+
+test('Convert 32 F to 0 C', () => {
+    const celsius = math.fahrenheitToCelsius(32) 
+    expect(celsius).toBe(0)
+})
+
+test('Convert 0 C to 32 F', () => {
+    const fahrenheit = math.celsiusToFahrenheit(0)
+    expect(fahrenheit).toBe(32)
+})
+
+/*asynchronous test
+//done is used to tell to the test that all the asynchronous code had executed 
+and so the test is really finisheds
+OLD WAY*/
+test('Async test demo', (done) => {
+    setTimeout(() => {
+        expect(1).toBe(1)
+        done()  //our test is really finished
+    })
+})
+
+test('should add two numbers', (done) => {
+    math.addAsync(2,3).then((sum) => {
+        expect(sum).toBe(5)
+        done()  //our test is really finished
+    })
+})
+
+//BETTER NEW WAY: using async await. we simple need to mark the test as async
+test('should add two numbers async/await', async () => {
+    var sum =await math.addAsync(2,3)
+    expect(sum).toBe(5)    
+})
+
+
 
